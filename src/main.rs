f6224a11ed68e52f3e303ed11940bb68e74fecf7
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2020-08-24 16:27:57 +0900

    Read file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let filename = env::args().nth(1).expect("no filename given");
-    println!("filename: {}", filename);
+    let content = fs::read_to_string(filename)
+       .expect("could not read file");
+
+    for line in content.lines() {
+        println!("{}", line);
+    }
 }
