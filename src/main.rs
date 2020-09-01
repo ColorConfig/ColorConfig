c54f76a5b59150f00ce47c5d2a7bd040a7064db5
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2020-09-01 23:42:00 +0900

    Read toml

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,52 @@
 use std::env;
 use std::fs;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+
+pub struct Solongo {
+    metadata: Metadata,
+    colors: Colors,
+}
+#[derive(Deserialize)]
+pub struct Colors {
+    primary: PrimaryColors,
+    normal: AnsiColors,
+    bright: AnsiColors,
+}
+
+#[derive(Deserialize)]
+struct Metadata {
+    name: String,
+    version: String,
+    author: String,
+    website: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct PrimaryColors {
+    background: String,
+    foreground: String,
+}
+
+#[derive(Deserialize)]
+struct AnsiColors {
+    black: String,
+    red: String,
+    green: String,
+    yellow: String,
+    blue: String,
+    magenta: String,
+    cyan: String,
+    white: String,
+}
 
 fn main() {
     let filename = env::args().nth(1).expect("no filename given");
     let content = fs::read_to_string(filename)
        .expect("could not read file");
 
-    for line in content.lines() {
-        println!("{}", line);
-    }
+    let content: Solongo = toml::from_str(&content).expect("toml error");
+
+    assert_eq!(content.colors.normal.black, "#13181b");
 }
