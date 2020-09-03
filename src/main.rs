9b3b0d56c64e853c26c8b7e73faed70712f992cd
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2020-09-03 23:17:40 +0900

    Jsonify

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 use std::env;
 use std::fs;
-use serde::Deserialize;
+use std::fs::File;
+use std::io::prelude::*;
+use serde::{Serialize, Deserialize};
 
-#[derive(Deserialize)]
-
+#[derive(Serialize, Deserialize)]
 pub struct Solongo {
     metadata: Metadata,
     colors: Colors,
 }
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct Colors {
     primary: PrimaryColors,
     normal: AnsiColors,
     bright: AnsiColors,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct Metadata {
     name: String,
     version: String,
@@ -23,13 +24,13 @@ struct Metadata {
     website: Option<String>,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct PrimaryColors {
     background: String,
     foreground: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct AnsiColors {
     black: String,
     red: String,
@@ -43,10 +44,15 @@ struct AnsiColors {
 
 fn main() {
     let filename = env::args().nth(1).expect("no filename given");
-    let content = fs::read_to_string(filename)
+    let content = fs::read_to_string(&filename)
        .expect("could not read file");
 
     let content: Solongo = toml::from_str(&content).expect("toml error");
+    let serialized = serde_json::to_string_pretty(&content)
+        .expect("failed to jsonify Solongo");
+
+    let mut file = File::create(format!("{}.json", &filename)).expect("failed to create file");
+    file.write_all(serialized.as_bytes()).expect("failed to write file");
 
     assert_eq!(content.colors.normal.black, "#13181b");
 }
