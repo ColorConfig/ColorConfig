e5710aeb5fe500f73b69738e880fd7ae64a6ce37
Jeong YunWon <jeong@youknowone.org>
2020-10-03 11:53:48 +0900

    Move Cli run interface to main

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,9 @@
-use anyhow::{Context, Result};
-use std::fs;
-
-mod cli;
+mod cli; // not a core module
 pub use cli::Cli;
 
 mod color_config;
 mod registry;
-mod target;
+pub mod target;
 
-use color_config::ColorConfig;
-
-pub fn run(args: Cli) -> Result<()> {
-    let content = fs::read_to_string(&args.color_config_path).with_context(|| {
-        format!(
-            "could not read file `{}`",
-            &args.color_config_path.display()
-        )
-    })?;
-
-    let registry = registry::TargetRegstry::with_bulitins();
-    let color_config: ColorConfig = toml::from_str(&content)
-        .with_context(|| format!("Failed to parse ColorConfig file `{}`", &content))?;
-
-    let from_config = registry
-        .get_from_config(&args.format.to_string())
-        .expect("tool must guarantee this unless we have plugin system");
-    let target = from_config(color_config);
-    let stem = args.color_config_path.file_stem().unwrap();
-    target.write_to_target_path(stem.to_str().unwrap(), false)?;
-
-    Ok(())
-}
+pub use color_config::ColorConfig;
+pub use registry::TargetRegstry;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
+use anyhow::{Context, Result};
+use colorconfig::{Cli, ColorConfig, TargetRegstry};
+use std::fs;
 use structopt::StructOpt;
 
 fn main() {
-    let args = colorconfig::Cli::from_args();
-    if let Err(err) = colorconfig::run(args) {
+    let args = Cli::from_args();
+    if let Err(err) = run(args) {
         eprintln!("Error: {:?}", err);
         std::process::exit(1);
     }
+}
+
+pub fn run(args: Cli) -> Result<()> {
+    let content = fs::read_to_string(&args.color_config_path).with_context(|| {
+        format!(
+            "could not read file `{}`",
+            &args.color_config_path.display()
+        )
+    })?;
+
+    let registry = TargetRegstry::with_bulitins();
+    let color_config: ColorConfig = toml::from_str(&content)
+        .with_context(|| format!("Failed to parse ColorConfig file `{}`", &content))?;
+
+    let from_config = registry
+        .get_from_config(&args.format.to_string())
+        .expect("tool must guarantee this unless we have plugin system");
+    let target = from_config(color_config);
+    let stem = args.color_config_path.file_stem().unwrap();
+    target.write_to_target_path(stem.to_str().unwrap(), false)?;
+
+    Ok(())
 }
