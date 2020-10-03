49e1cd291034c0cb1fcf39997b053cdc05865e42
Jeong YunWon <jeong@youknowone.org>
2020-10-03 12:27:11 +0900

    Source trait to read from str

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use anyhow::{Context, Result};
-use colorconfig::{Cli, ColorConfig, TargetRegstry};
-use std::fs;
+use anyhow::Result;
+use colorconfig::{target::Source, Cli, ColorConfig, TargetRegstry};
 use structopt::StructOpt;
 
 fn main() {
@@ -12,23 +11,19 @@ fn main() {
 }
 
 pub fn run(args: Cli) -> Result<()> {
-    let content = fs::read_to_string(&args.color_config_path).with_context(|| {
-        format!(
-            "could not read file `{}`",
-            &args.color_config_path.display()
-        )
-    })?;
-
     let registry = TargetRegstry::with_bulitins();
-    let color_config: ColorConfig = toml::from_str(&content)
-        .with_context(|| format!("Failed to parse ColorConfig file `{}`", &content))?;
-
+    let color_config = ColorConfig::from_path(&args.color_config_path)?;
     let from_config = registry
         .get_from_config(&args.format.to_string())
         .expect("tool must guarantee this unless we have plugin system");
     let target = from_config(color_config);
-    let stem = args.color_config_path.file_stem().unwrap();
-    target.write_to_target_path(stem.to_str().unwrap(), false)?;
+    let color_name = args
+        .color_config_path
+        .file_stem()
+        .unwrap()
+        .to_str()
+        .unwrap();
+    target.write_to_target_path(color_name, false)?;
 
     Ok(())
 }
--- src/target/interface.rs
+++ src/target/interface.rs
@@ -6,6 +6,13 @@ pub trait TargetImpl: Sized {
     const NAME: &'static str;
     const EXTENSION: &'static str;
     fn to_string_pretty(&self) -> anyhow::Result<String>;
+    fn target_path(name: &str) -> PathBuf {
+        let mut path = PathBuf::new();
+        path.push(Self::NAME);
+        path.set_file_name(name);
+        path.set_extension(Self::EXTENSION);
+        path
+    }
 }
 
 pub trait Target
@@ -26,11 +33,7 @@ where
     T: TargetImpl + 'static,
 {
     fn target_path(&self, name: &str) -> PathBuf {
-        let mut path = PathBuf::new();
-        path.push(Self::NAME);
-        path.set_file_name(name);
-        path.set_extension(Self::EXTENSION);
-        path
+        Self::target_path(name)
     }
     fn write_to_path(&self, path: &Path, overwrite: bool) -> anyhow::Result<()> {
         if !overwrite {
@@ -41,5 +44,20 @@ where
         file.write_all(content.as_bytes())
             .context("Failed to write file.")?;
         Ok(())
+    }
+}
+
+pub trait Source: Sized {
+    fn from_content(content: &str) -> anyhow::Result<Self>;
+    fn from_path(path: &Path) -> anyhow::Result<Self> {
+        let content = std::fs::read_to_string(path)
+            .with_context(|| format!("could not read file `{}`", path.display()))?;
+        Self::from_content(&content)
+    }
+    fn from_target_path(name: &str) -> anyhow::Result<Self>
+    where
+        Self: TargetImpl,
+    {
+        Self::from_path(&Self::target_path(name))
     }
 }
